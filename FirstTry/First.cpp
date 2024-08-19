836452c8356b2ff0ffe09d20f35e9c3740bb72fc
yp6aHouD <danilnelubin@yandex.ru>
2024-08-19 23:41:22 +0800

    nomsg

--- FirstTry/First.cpp
+++ FirstTry/First.cpp
@@ -10,7 +10,7 @@ int main()
     int v1 = 0, v2 = 0;
 
     std::cin >> v1 >> v2;
-    std:: cout << "v1 = " << v1 << ", v2 = " << v2 << std::endl;
+    std::cout << "v1 = " << v1 << ", v2 = " << v2 << std::endl;
 
     return 0;
 }
